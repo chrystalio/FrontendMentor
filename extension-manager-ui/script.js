125d3e1237acff34e0335b33506fd76c7cd8e1e4
Chrystalio <chrystalio.k@outlook.com>
2025-04-01 16:07:21 +0700

    Implement focus and active state for remove button

--- extension-manager-ui/script.js
+++ extension-manager-ui/script.js
@@ -30,21 +30,25 @@ const renderData = (extensions) => {
 
         card.innerHTML = `
         <div class="flex">
-          <div class="me-3 pt-1">
-            <img src="${ext.logo}" alt="${ext.name}" class="max-w-20">
-          </div>
-          <div>
-            <p class="text-lg font-bold">${ext.name}</p>
-            <p class="text-sm py-1">${ext.description}</p>
-          </div>
-        </div>
-        <div class="pt-6 lg:pt-10 flex justify-between items-center">
-          <div class="rounded-2xl border border-slate-300 py-1 px-3 text-sm hover:bg-[#C7221A] hover:text-white transition transition-colors duration-300 ease-in-out cursor-pointer">Remove</div>
-          <label class="relative inline-flex items-center cursor-pointer w-9 h-5">
-            <input type="checkbox" class="sr-only peer" ${ext.isActive ? 'checked' : ''}>
-            <div class="w-full h-full bg-gray-300 rounded-full peer-checked:bg-[#C7221A] transition-colors duration-300"></div>
-            <div class="absolute top-0.5 left-0.5 w-4 h-4 bg-white rounded-full transition-all duration-300 peer-checked:translate-x-[1rem]"></div>
-          </label>
+            <div class="me-3 pt-1">
+                <img src="${ext.logo}" alt="${ext.name}" class="max-w-20">
+            </div>
+            <div>
+                <p class="text-lg font-bold">${ext.name}</p>
+                <p class="text-sm py-1">${ext.description}</p>
+            </div>
+            </div>
+
+            <div class="pt-6 lg:pt-10 flex justify-between items-center">
+                <button tabindex="0" class="rounded-2xl focus:outline-2 focus:outline-offset-2 focus:outline-[#C7221A] focus:bg-slate-200 focus:border-slate-1-0 active:bg-[#C7221A] border border-slate-300 text-[#091540] py-1 px-4 text-sm hover:bg-[#C7221A] hover:text-white transition-colors duration-300 ease-in-out cursor-pointer">
+                    Remove
+                </button>
+
+            <label class="relative inline-flex items-center cursor-pointer w-9 h-5">
+                <input type="checkbox" class="sr-only peer" ${ext.isActive ? 'checked' : ''}>
+                <div class="w-full h-full bg-gray-300 border border-[#C7221A] peer-checked:bg-[#C7221A] rounded-full transition-colors duration-300"></div>
+                <div class="absolute top-0.5 left-0.5 w-4 h-4 bg-white rounded-full transition-all duration-300 peer-checked:translate-x-[1rem] border border-white"></div>
+            </label>
         </div>
       `;
 
